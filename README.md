3f1a8b3ab89435dbae69480af5ee31ec4901a077
Asem-Badr <70919962+Asem-Badr@users.noreply.github.com>
2022-07-31 18:36:30 +0200

    Add files via upload

---
+++ README.md
@@ -0,0 +1,5 @@
+this folder contains two programs :
+echoserveri which is a concurrent echo server 
+echoclient which is the program for the connects to the server
+the server echos what the client sends to it and print the number of lines on the text requested to be echoed 
+th server is able to serve multible clients as it generates a child proccess for each request to handle it 
---
+++ echoserveri.c
@@ -0,0 +1,42 @@
+/* 
+ * echoserveri.c - An iterative echo server 
+ */ 
+/* $begin echoserverimain */
+#include "csapp.h"
+
+void echo(int connfd);
+
+void sigchld_handler(int sig)
+{
+    while(waitpid(-1,0,WNOHANG)>0);
+    return;
+}
+
+int main(int argc, char **argv) 
+{
+    int listenfd, connfd;
+    socklen_t clientlen;
+    struct sockaddr_storage clientaddr;  /* Enough space for any address */  //line:netp:echoserveri:sockaddrstorage
+    char client_hostname[MAXLINE], client_port[MAXLINE];
+
+    if (argc != 2) {
+	fprintf(stderr, "usage: %s <port>\n", argv[0]);
+	exit(0);
+    }
+    Signal(SIGCHLD,sigchld_handler);//invoking the signal handler
+
+    listenfd = Open_listenfd(argv[1]);
+    while (1) {
+	clientlen = sizeof(struct sockaddr_storage); 
+	connfd = Accept(listenfd, (SA *)&clientaddr, &clientlen);
+    printf("Connected to (%s, %s)\n", client_hostname, client_port);
+    if(fork()==0){
+    Close(listenfd);
+    echo(connfd);
+	Close(connfd);
+    exit(0);
+    }
+    Close(connfd);
+    }
+}
+/* $end echoserverimain */
